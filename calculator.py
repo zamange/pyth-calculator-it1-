f02aad2146ae8903134e4c75205017ac3bd02475
WTC <zngeyane023@student.wethinkcode.co.za>
2024-04-22 13:30:45 +0200

    user input

--- calculator.py
+++ calculator.py
@@ -1,23 +1,29 @@
 # unnittests, add, sub, mult, div, main loop, ui
 
 
-def add(x, y):
-    sum = x + y
+def user_input():
+    num1 = print("Enter the first number: ")
+    num2 = print("Enter the second number: ")
+    return num1, num2
+
+
+def add(num1, num2):
+    sum = num1 + num2
     print(sum)
-    return sum
+    return sum 
 
 
 
-def subtract(x, y):
-    result = x - y
+def subtract(num1, num2):
+    result = num1 - num2
     print (result)
     return result
 
 
 
-def multiply(x, y):
+def multiply(num1, num2):
     try:
-        product = x * y
+        product = num1 * num2
     except TypeError:
         return "TypeError"
     
@@ -25,9 +31,9 @@ def multiply(x, y):
     
 
 
-def divide(x, y):
+def divide(num1, num2):
     try:
-        quotient = x / y
+        quotient = num1 / num2
         return quotient
     except ZeroDivisionError:
         raise ZeroDivisionError("Cannot divide by zero")
